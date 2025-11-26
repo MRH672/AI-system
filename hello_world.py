a5082844940f57e58f3e36c519b9893948a44513
MRH672 <hassan.hesham672@gmail.com>
2025-11-26 20:10:42 +0200

    badr(hello world)

---
+++ hello_world.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+برنامج Hello World بسيط
+Simple Hello World Program
+"""
+
+def main():
+    print("Hello, World!")
+    print("مرحباً بالعالم!")
+
+if __name__ == "__main__":
+    main()
+
